fc9ec349deff78226a133bffde8d9260f2207618
gromv <gromvrs@yahoo.fr>
2013-03-12 21:21:20 +0100

    Add a320 cross compilation file

---
+++ a320.sh
@@ -0,0 +1,2 @@
+#export PATH=$PATH:/opt/opendingux-toolchain/usr/bin
+make linux CC="mipsel-unknown-linux-uclibc-gcc" AR="mipsel-unknown-linux-uclibc-ar rcu" RANLIB="mipsel-unknown-linux-uclibc-ranlib"
